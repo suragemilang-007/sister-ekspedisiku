986af45c574a9579f6202ebe2122fb73edfd6a19
mamaztrian <tri.ant45.ta@gmail.com>
2025-06-23 13:08:55 +0700

    Perbaiki Title Manajemen Admin

--- resources/views/admin/pengguna/index.blade.php
+++ resources/views/admin/pengguna/index.blade.php
@@ -8,7 +8,7 @@
     <!-- Recent Shipments -->
     <div class="card mb-4">
         <div class="card-header d-flex justify-content-between align-items-center">
-            <h5 class="mb-0">Histori Pengiriman</h5>
+            <h5 class="mb-0">Manajemen Admin</h5>
             <a href="/admin/pengguna/create" class="btn btn-primary">
                 <i class="fas fa-plus me-2"></i> Tambah Admin
             </a>
