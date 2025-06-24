8a877517a370907152806c7f989a9be2b1e71cf9
suragemilang-007 <ignassurya450@gmail.com>
2025-06-24 21:29:46 +0700

    Add kecamatan selection to create alamat tujuan form

--- resources/views/alamat-tujuan/create.blade.php
+++ resources/views/alamat-tujuan/create.blade.php
@@ -20,7 +20,13 @@
             </div>
             <div class="mb-3">
                 <label>Kecamatan</label>
-                <input type="text" class="form-control" name="kecamatan" required>
+                
+                <select class="form-select" id="kecamatan" name="kecamatan" required>
+                                            <option value="">Pilih kecamatan asal...</option>
+                                            @foreach($kecamatanTujuan as $kecamatan)
+                                            <option value="{{ $kecamatan }}">{{ $kecamatan }}</option>
+                                            @endforeach
+                                        </select>
             </div>
             <div class="mb-3">
                 <label>Kode Pos</label>
@@ -35,11 +41,30 @@
     </div>
 </div>
 @endsection
+<!-- Styles -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" />
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/css/select2.min.css" />
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.min.css" />
+<!-- Or for RTL support -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.rtl.min.css" />
 
+<!-- Scripts -->
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.0/dist/jquery.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/js/select2.full.min.js"></script>
 @section('scripts')
 <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<!-- Select2 JS -->
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 <script>
+
+$( '#kecamatan' ).select2( {
+    theme: "bootstrap-5",
+    width: $( this ).data( 'width' ) ? $( this ).data( 'width' ) : $( this ).hasClass( 'w-100' ) ? '100%' : 'style',
+    placeholder: $( this ).data( 'placeholder' ),
+} );
 document.getElementById('alamatForm').addEventListener('submit', function(e) {
     e.preventDefault();
 
