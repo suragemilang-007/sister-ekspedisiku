2311d53f235a2f5daa6e0bb9383e32c9827625c5
mamaztrian <tri.ant45.ta@gmail.com>
2025-06-29 20:46:05 +0700

    Dashboard Admin Fixes #74

--- app/Http/Controllers/adminController.php
+++ app/Http/Controllers/adminController.php
@@ -37,9 +37,7 @@ class adminController extends Controller
         ];
 
         // Recent shipments
-        $recent_shipments = Pengiriman::with(['alamatTujuan', 'layananPaket', 'pelacakan', 'pengguna', 'alamatPenjemputan'])
-            ->where('status', '!=', 'DIBATALKAN')
-            ->where('status', '!=', 'DITERIMA') // Exclude cancelled shipments
+        $recent_shipments = Pengiriman::with(['alamatTujuan', 'layananPaket', 'pelacakan', 'pengguna', 'alamatPenjemputan'])// Exclude cancelled shipments
             ->orderBy('created_at', 'desc')
             ->take(5)
             ->get();
