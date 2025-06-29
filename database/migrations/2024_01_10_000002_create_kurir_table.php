8e29004337a9cf217248ea03f6f08166143bc19b
suragemilang-007 <ignassurya450@gmail.com>
2025-06-29 16:22:25 +0700

    update kurir migration and seeder to include kendaraan field

--- database/migrations/2024_01_10_000002_create_kurir_table.php
+++ database/migrations/2024_01_10_000002_create_kurir_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up()
     {
         Schema::create('kurir', function (Blueprint $table) {
@@ -17,6 +16,7 @@ return new class extends Migration
             $table->string('foto')->nullable();
             $table->string('sandi_hash');
             $table->enum('status', ['AKTIF', 'NONAKTIF']);
+            $table->enum('kendaraan', ['MOTOR', 'MOBIL'])->default('MOTOR');
             $table->timestamp('created_at');
         });
     }
